e92a9e4fec8f5b7a5ea293bcde035c23569a8e5c
minsin56 <42984786+minsin56@users.noreply.github.com>
2020-10-29 23:40:16 -0500

    c plus fuck

---
+++ CPlusFuck.h
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdio>
+
+//essentials
+#define Then {
+#define End }
+#define Stop ;
+
+// ()
+#define IfStart (
+#define IfEnd )
+#define FunctionParamStart (
+#define FunctionParamEnd )
+#define define ForStart (
+#define ForEnd )
+
+
+//statements
+#define When while
+#define ForLoop for
+#define IfStatement if
+#define TryStatement try
+#define CatchStatement catch
+
+//functions
+#define FuckUpMyPC for(int i = 0;i<99999999999999999999;i++){std::cout << "CPlusFuck" << std::endl;}
+#define Print printf
---
+++ Test.cpp
@@ -0,0 +1,10 @@
+#include "CPlusFuck.h"
+
+int main() Then
+
+    IfStatement IfStart true IfEnd Then
+        Print FunctionParamStart "hello" FunctionParamEnd Stop
+    End
+
+    return 1 Stop
+End
